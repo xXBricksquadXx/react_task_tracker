8dceb2990d185795b3d2db3adb4ca1bc16c1a644
Colby <colby88cardell@gmail.com>
2021-02-06 16:45:25 -0500

    Import 'PropTypes' to 'Button.js'

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,21 @@
+import PropTypes from 'prop-types'
+
 const Button = ({ color, text }) => {
-    return <button style={{ backgroundColor: color}}
+    return (
+         <button style={{ backgroundColor: color}}
     className='btn'>
     {text}
     </button>
-    
+    )
+}
+
+Button.defaultProps = {
+    color: 'steelblue',
+}
+
+Button.propTypes = {
+    text: PropTypes.string,
+    color: PropTypes.string
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = ({title}) => {
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Hello'/>
+            <Button color='green' text='Add'/>
         </header>
     )
 }
