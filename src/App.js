0c8a3f7d8885f4e4d06a2e101efeb4e1123244fa
Colby <colby88cardell@gmail.com>
2021-02-06 20:52:34 -0500

    Created 'AddTask' function & passed as a prop

--- src/App.js
+++ src/App.js
@@ -25,6 +25,11 @@ function App() {
       }
     ])
 
+// Add Task
+const addTask = (task) => {
+  console.log(task);
+}    
+
 // Delete Task
     const deleteTask = (id) => {
       setTasks(tasks.filter((task) => task.id !== 
@@ -43,7 +48,7 @@ function App() {
   return (
     <div className="container">
      <Header />
-     <AddTask />
+     <AddTask onAdd={addTask}/>
      {tasks.length > 0 ? (
      <Tasks tasks={tasks} onDelete=
      {deleteTask} onToggle={toggleReminder} />
