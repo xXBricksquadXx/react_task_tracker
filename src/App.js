2618d6c86363e5f3d35c72f98cc18574320b48f2
Colby <colby88cardell@gmail.com>
2021-02-06 19:08:05 -0500

    Added 'if' 'else' for message if nothing is displayed

--- src/App.js
+++ src/App.js
@@ -34,8 +34,12 @@ function App() {
   return (
     <div className="container">
      <Header />
+     {tasks.length > 0 ? (
      <Tasks tasks={tasks} onDelete=
      {deleteTask} />
+     ): (
+       'No Tasks To Show'
+     )}
      
     </div>
   );
