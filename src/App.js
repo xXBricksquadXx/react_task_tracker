dee09a9e119dc75e47e3b6e308dfa60825754204
Colby <colby88cardell@gmail.com>
2021-02-06 21:08:02 -0500

    Show AddTask state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState
+  (false)
   const [tasks, setTasks] = useState([
     {
         id: 1,
@@ -51,7 +53,7 @@ const addTask = (task) => {
   return (
     <div className="container">
      <Header />
-     <AddTask onAdd={addTask}/>
+     {showAddTask && <AddTask onAdd={addTask}/>}
      {tasks.length > 0 ? (
      <Tasks tasks={tasks} onDelete=
      {deleteTask} onToggle={toggleReminder} />
