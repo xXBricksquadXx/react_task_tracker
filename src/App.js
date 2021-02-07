0260d56e3de8fa490d156cc5599ebca292147738
Colby <colby88cardell@gmail.com>
2021-02-06 21:23:44 -0500

    Pass in 'showAdd' prop

--- src/App.js
+++ src/App.js
@@ -53,7 +53,8 @@ const addTask = (task) => {
   return (
     <div className="container">
      <Header onAdd={() => setShowAddTask
-     (!showAddTask)} />
+     (!showAddTask)}
+     showAdd={showAddTask} />
      {showAddTask && <AddTask onAdd={addTask}/>}
      {tasks.length > 0 ? (
      <Tasks tasks={tasks} onDelete=
